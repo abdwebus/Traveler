ff2077c10cd82f0617f4f8dbe6d627baca60126d
Abdulwahab <abdwebus@hotmail.com>
2019-03-29 09:48:11 -0600

    Separated Header and Footer from index.php

--- templetes/footer.php
+++ templetes/footer.php
@@ -1,3 +1,4 @@
-<?php 
-	// Footer
-?>
+<footer class="container">
+	<p class="float-right"><a href="#">Back to top</a></p>
+	<p>© 2017-2019 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+</footer>
