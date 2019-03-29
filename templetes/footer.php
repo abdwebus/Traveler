12cbed8a7eaf9a6fb0d61916a77e06f97285e11e
liam34 <48293076+liam34@users.noreply.github.com>
2019-03-29 15:56:33 -0600

    changes made

--- templetes/footer.php
+++ templetes/footer.php
@@ -1,4 +1,32 @@
+
+
+<link href="css/styleliam.css" rel="stylesheet" type="text/css" />
+
 <footer class="container">
+
+<div class="container-top">
+
+<div><img src="traveladvisor.png" style="width:200px;height:60px;"></div>
+<div><img src="expedia.png" style="width:200px;height:60px;"></div>
+<div><img src="traveladvisor.png" style="width:200px;height:60px;"></div>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+</div>
+<br>
+
+<p>Connect With Us</p>
+<i class="fab fa-facebook fa-3x"></i>
+<i class="fab fa-twitter fa-3x"></i>
+<i class="fab fa-instagram fa-3x"></i>
+<i class="fab fa-youtube-square fa-3x"></i>
 	<p class="float-right"><a href="#">Back to top</a></p>
-	<p>© 2017-2019 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+	<p>© 2017-2020 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
 </footer>
