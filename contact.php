432df918770f3aff6f0ff9a7fff1cdf425b70f14
liam34 <48293076+liam34@users.noreply.github.com>
2019-04-06 00:25:46 -0600

    changes contact page

--- contact.php
+++ contact.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" type="text/css" href="css/ariel.css">
 	<link rel="stylesheet" type="text/css" href="css/styleliam.css">
 	<link rel="stylesheet" type="text/css" href="css/contact.css">
+	<link rel="stylesheet" type="text/css" href="Traveler\images.jpg">
 
 
 
@@ -65,9 +66,42 @@
 
 			</div>
 		</div> 
+<div class="row">
+	<div class="card" style="width: 18rem;">
+  		<img class="card-img-top" src="..." alt="Card image cap">
+  			<div class="card-body">
+    			<h5 class="card-title">Card title</h5>
+    				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    					<a href="#" class="btn btn-primary">Go somewhere</a>
+  							</div>
+  								<div class="card" style="width: 18rem;">
+  									<img class="card-img-top" src="..." alt="Card image cap">
+  								<div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   					 <a href="#" class="btn btn-primary">Go somewhere</a>
+  			</div>
+		</div>
+	</div>
+</div>
+		<!-- <div class="row">
 					<div class="container">
-						
-					
+					<div class="card" style="width: 18rem;">
+  <img src="bestvacations.png" class="card-img-top" alt="downtown">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div> -->
+  </div>				
 			     	</div>
 
 	</div> <!-- Container end -->
