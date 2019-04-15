53f14a620ce26278b433eeb4868a233866fb475a
Abdulwahab <abdwebus@hotmail.com>
2019-04-15 13:51:08 -0600

    Moved to local database

--- model/dbConnection.php
+++ model/dbConnection.php
@@ -1,9 +1,17 @@
 <?php
 // Author: Abdulwahab Alansari 
 
-// Create connection to remote database 
-$connect = mysqli_connect('remotemysql.com', 'oeOIZTGN6X', 'SEZXQjCoW0', 'oeOIZTGN6X', '3306');
+// Connecting to local database
+$connect = mysqli_connect('localhost', 'root', '', 'travelexperts');
 if (!$connect) {
 	die(mysql_error());
 }
+
+
+// Uncomment below to connect remote db
+
+// $connect = mysqli_connect('remotemysql.com', 'oeOIZTGN6X', 'SEZXQjCoW0', 'oeOIZTGN6X', '3306');
+// if (!$connect) {
+// 	die(mysql_error());
+// }
 ?>
--- models/connect.php
+++ models/connect.php
@@ -2,11 +2,23 @@
 <!-- Establish connection to DB -->
 <?php
 
-    $servername = "remotemysql.com";
-    $username = "oeOIZTGN6X";
-    $password = "SEZXQjCoW0";
-    $dbname = "oeOIZTGN6X";
-    $connect = mysqli_connect($servername, $username, $password, $dbname, '3306') or die("Connection failed: " . mysqli_connect_error());
+	// Connecting to local database
+
+	$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "travelexperts";
+
+    $connect = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+
+	// Uncomment below to connect to remote db 
+
+    // $servername = "remotemysql.com";
+    // $username = "oeOIZTGN6X";
+    // $password = "SEZXQjCoW0";
+    // $dbname = "oeOIZTGN6X";
+
+    // $connect = mysqli_connect($servername, $username, $password, $dbname, '3306') or die("Connection failed: " . mysqli_connect_error());
 
 ?>
 
