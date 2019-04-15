3216727fb623a6f11ca3044d55f92a6d99c30a46
arielfc <48219012+arielfc@users.noreply.github.com>
2019-04-14 22:31:41 -0600

    mouseover animation on front page packages

--- templates/rowPackageLeft.php
+++ templates/rowPackageLeft.php
@@ -4,8 +4,14 @@
 		<h2 class="featurette-heading"><?php echo $row['PkgName'];  ?></h2>
 		<p class="lead"><?php echo $row['PkgDesc']; ?></p>
 	</div>
-	<div class="col-md-5 order-md-1">
+	<div class="hovereffect col-md-5 order-md-1">
 		<img class="frontpage bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="<?php echo $row['PkgImgUrl']; ?>" width="500" height="500" preserveAspectRatio="xMidYMid slice" focusable="false">
+		<div class="overlay">
+			<h2>$<?php echo $row['PkgBasePrice']; ?></h2>
+			<p>
+				<button role="button" class="btn btn-outline-light" onclick="document.location.href='destinations.php'">BOOK NOW</button>
+			</p>
+		</div>
 	</div>
 </div>
 <hr class="featurette-divider">
--- templates/rowPackageRight.php
+++ templates/rowPackageRight.php
@@ -3,8 +3,14 @@
 		<h2 class="featurette-heading"><?php echo $row['PkgName'];  ?></h2>
 		<p class="lead"><?php echo $row['PkgDesc']; ?></p>
 	</div>
-	<div class="col-md-5">
+	<div class="hovereffect col-md-5">
 		<img class="frontpage bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="<?php echo $row['PkgImgUrl']; ?>" width="500" height="500" preserveAspectRatio="xMidYMid slice" focusable="false">
+		<div class="overlay">
+			<h2>$<?php echo $row['PkgBasePrice']; ?></h2>
+			<p>
+			<button role="button" class="btn btn-outline-light" onclick="document.location.href='destinations.php'">BOOK NOW</button>
+			</p>
+		</div>
 	</div>
 </div>
 <!-- <hr class="featurette-divider"> -->
