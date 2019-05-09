dc1d0223abce445dc03ad1f903917a2ae1d44cba
Abdulwahab <abdwebus@hotmail.com>
2019-05-08 23:39:13 -0600

    Cleaned the footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,90 +1,56 @@
+<link rel="stylesheet" href="css/styleliam.css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+<footer class="footer-distributed">
+	<div class="footer-left">
+		<div>
+			<img height="95" width="95" src="images/logo_transparent.png">
+		</div>
+		<p class="footer-links">
+			<a href="index.php">Home</a>
+			·
+			<a href="destinations.php">Packages</a>
+			·
+			<a href="about.php">About</a>
+			·
+			<a href="faq.php">FAQ</a>
+			·
+			<a href="contact.php">Contact</a>
+		</p>
+		<p class="footer-company-name">Traveler &copy; 2019-2020</p>
+	</div>
 
+	<div class="footer-center">
+		<div>
+			<i class="fa fa-map-marker"></i>
+			<p><span>10230 Jasper Ave, Edmonton, AB T5J 4P6</span> Downtown Edmonton Alberta, Canada</p>
+		</div>
+		<div>
+			<i class="fa fa-phone"></i>
+			<p>1-877-233-4567</p>
+		</div>
+		<div>
+			<i class="fa fa-envelope"></i>
+			<p><a href="mailto:support@company.com">customerservice@traveler.com</a></p><br>
+		</div>
+	</div>
 
-
-	
-	<link rel="stylesheet" href="css/styleliam.css">
-	
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-
-	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
-
-	<footer class="footer-distributed">
-
-			<div class="footer-left">
-
-				
-				<div>
-				<img height="95" width="95" src="images/logo_transparent.png">
-				
-			
-				
-				</div>
-
-				<p class="footer-links">
-					<a href="index.php">Home</a>
-					·
-				
-					<a href="destinations.php">Packages</a>
-
-					·
-					<a href="about.php">About</a>
-					·
-					<a href="faq.php">FAQ</a>
-					·
-					<a href="contact.php">Contact</a>
-				</p>
-
-				<p class="footer-company-name">The Traveler &copy; 2019-2020</p>
-			</div>
-			
-			<div class="footer-center">
-
-				<div>
-
-				
-					<i class="fa fa-map-marker"></i>
-					<p><span>10230 Jasper Ave, Edmonton, AB T5J 4P6</span> Downtown Edmonton Alberta, Canada</p>
-				</div>
-
-				<div>
-					<i class="fa fa-phone"></i>
-					<p>1-877-233-4567</p>
-				</div>
-
-				<div>
-					<i class="fa fa-envelope"></i>
-					<p><a href="mailto:support@company.com">customerservice@traveler.com</a></p><br>
-					<!-- <p><img height="110" width="100" src="images/expedia.png"></p> -->
-				</div>
-
-			</div>
-
-			<div class="footer-right">
-
-				<p class="footer-company-about">
-					<span>About the company</span>
-					New Life, New Discoveries, and many more sweet memories. Bon Voyage!
-				</p>
-
-				<div class="footer-icons">
-
-					<a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-					<a href="#"><i class="fa fa-github"></i></a>
-				<div>
+	<div class="footer-right">
+		<p class="footer-company-about">
+			<span>About the company</span>
+			New Life, New Discoveries, and many more sweet memories. Bon Voyage!
+		</p>
+		<div class="footer-icons">
+			<a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a>
+			<a href="#"><i class="fa fa-twitter"></i></a>
+			<a href="#"><i class="fa fa-linkedin"></i></a>
+			<a href="#"><i class="fa fa-github"></i></a>
+			<div>
 				<img height="95" width="95" src="https://www.emirates.com/ca/english/images/Apple_EN_tcm302-4143604.svg">
 				<img height="95" width="95" src="https://www.emirates.com/ca/english/images/Google_EN_tcm302-4143606.svg">
 				<!-- <img height="95" width="95" src="https://www.emirates.com/ca/english/images/Apex_Badge_EN_tcm302-5456919.svg">
-				<img height="50" width="50" src="images/bestvacations.png"> -->
-				
-				
-				</div>
-
+					<img height="50" width="50" src="images/bestvacations.png"> -->
 			</div>
-
-		</footer>
-
-
- 
- 
+		</div>
+	</div>
+</footer>
