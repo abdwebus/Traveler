693369eff65ac90fe6e4bb1bb86502216e881b93
Abdulwahab <abdwebus@hotmail.com>
2019-05-08 23:02:51 -0600

    Removed newletter section and fixed management names

--- contact.php
+++ contact.php
@@ -53,14 +53,14 @@
 	<?php include 'signup.php' ?>
 
 	<main class="container">
-	<div class="row d-flex justify-content-center section-gap">
-	<div class="menu-content col-lg-8">
-		<div class="title text-center">
-			<h1 class="mb-10">Travel Agencies</h1>
-			<p class="lightertext">Where Customers Come First</p>
+		<div class="row d-flex justify-content-center section-gap">
+			<div class="menu-content col-lg-8">
+				<div class="title text-center">
+					<h1 class="mb-10">Travel Agencies</h1>
+					<p class="lightertext">Where Customers Come First</p>
+				</div>
+			</div>
 		</div>
-	</div>
-	</div>
 		<!-- agencies -->
 		<?php
 		include_once("models/connect.php");
@@ -99,14 +99,14 @@
 
 	<section id="team" class="pb-5">
 		<div class="container">
-		<div class="row d-flex justify-content-center pb-70">
-		<div class="menu-content col-lg-8">
-			<div class="title text-center">
-				<h1 class="mb-10">Travel Agents</h1>
-				<p class="lightertext">Your Guides To Your Dream Getaway</p>
+			<div class="row d-flex justify-content-center pb-70">
+				<div class="menu-content col-lg-8">
+					<div class="title text-center">
+						<h1 class="mb-10">Travel Agents</h1>
+						<p class="lightertext">Your Guides To Your Dream Getaway</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		</div>
 			<div class="row">
 				<?php while( $record = mysqli_fetch_assoc($results) ){?>
 					<!-- Team member -->
@@ -119,157 +119,122 @@
 											<p><img class=" img-fluid" src="<?php echo $record['AgtImg']; ?>" alt="card image"></p>
 											<h4 class="card-title"><?php echo $record['AgtFirstName']; ?> <?php echo $record['AgtLastName']; ?></h4>
 											<p class="card-text"><?php echo $record['AgtBusPhone']; ?></p>
-												<a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
-											</div>
+											<a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
 										</div>
 									</div>
+								</div>
 
-									<div class="backside">
-										<div class="card">
-											<div class="card-body text-center mt-4">
-												<h4 class="card-title"><?php echo $record['AgtPosition']; ?></h4>
-												<p class="card-text"><?php echo $record['AgtEmail']; ?></p>
-												<ul class="list-inline">
-													<li class="list-inline-item">
-														<a class="social-icon text-xs-center" target="_blank" href="#">
-															<i class="fa fa-facebook"></i>
-														</a>
-													</li>
-													<li class="list-inline-item">
-														<a class="social-icon text-xs-center" target="_blank" href="#">
-															<i class="fa fa-twitter"></i>
-														</a>
-													</li>
-													<li class="list-inline-item">
-														<a class="social-icon text-xs-center" target="_blank" href="#">
-															<i class="fa fa-skype"></i>
-														</a>
-													</li>
-													<li class="list-inline-item">
-														<a class="social-icon text-xs-center" target="_blank" href="#">
-															<i class="fa fa-google"></i>
-														</a>
-													</li>
-												</ul>
-											</div>
+								<div class="backside">
+									<div class="card">
+										<div class="card-body text-center mt-4">
+											<h4 class="card-title"><?php echo $record['AgtPosition']; ?></h4>
+											<p class="card-text"><?php echo $record['AgtEmail']; ?></p>
+											<ul class="list-inline">
+												<li class="list-inline-item">
+													<a class="social-icon text-xs-center" target="_blank" href="#">
+														<i class="fa fa-facebook"></i>
+													</a>
+												</li>
+												<li class="list-inline-item">
+													<a class="social-icon text-xs-center" target="_blank" href="#">
+														<i class="fa fa-twitter"></i>
+													</a>
+												</li>
+												<li class="list-inline-item">
+													<a class="social-icon text-xs-center" target="_blank" href="#">
+														<i class="fa fa-skype"></i>
+													</a>
+												</li>
+												<li class="list-inline-item">
+													<a class="social-icon text-xs-center" target="_blank" href="#">
+														<i class="fa fa-google"></i>
+													</a>
+												</li>
+											</ul>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<!-- ./Team member -->
-					<?php }?>
-				</div>
-			</div>
-		</section>
-		<!-- insert map here -->
-		<section class="container">
-
-			<!-- team traveler -->
-
-			<div class="container content">
-				<div class="heading">
-					<h2><strong>Our Management team</strong></h2>
-					<p>Its Our Pleasure to do business with you</p>
-				</div><!-- //end heading -->
-
-	<div class="row">
-        <div class="col-sm-4">
-            <div class="team-members">
-                <div class="team-avatar">
-                    <img class="img-responsive" src="images/member1.png" alt="">
-                </div>
-                <div class="team-desc">
-                    <h4>Paul Stern</h4>
-                    <span>Marketing</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="team-members">
-                <div class="team-avatar">
-                    <img class="img-responsive" src="images/member2.png" alt="">
-                </div>
-                <div class="team-desc">
-                    <h4>Sarah Parker</h4>
-                    <span>Founder</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="team-members">
-                <div class="team-avatar">
-                    <img class="img-responsive" src="images/member3.png" alt="">
-                </div>
-                <div class="team-desc">
-                    <h4>steve Jobs</h4>
-                    <span>Director</span>
-                </div>
-            </div>
-        </div>
-    </div><!-- //end row -->
-</div>
-
-			<br>
-			<br>
-			<center>
-				<strong>Powered by <a href="index.php" target="_blank">Traveler@Company</a></strong>
-			</center>
-			<br>
-			<br>
-		</section>
-		<!-- email newsletter -->
-
-		<section class="subscribe-area pb-50 pt-70">
-			<div class="container">
-				<div class="row">
-
-					<div class="col-md-4">
-						<div class="subscribe-text mb-15">
-							<span>JOIN OUR NEWSLETTER</span>
-							<h2>subscribe newsletter</h2>
-						</div>
 					</div>
-					<div class="col-md-8">
-						<div class="subscribe-wrapper subscribe2-wrapper mb-15">
-							<div class="subscribe-form">
-								<form action="#">
-									<input placeholder="enter your email address" type="email">
-									<button>subscribe <i class="fas fa-long-arrow-alt-right"></i></button>
-								</form>
-							</div>
+					<!-- ./Team member -->
+				<?php }?>
+			</div>
+		</div>
+	</section>
+	<!-- insert map here -->
+	<section class="container">
+
+		<!-- team traveler -->
+
+		<div class="container content">
+			<div class="heading">
+				<h2><strong>Our Management team</strong></h2>
+				<p>Its Our Pleasure to do business with you</p>
+			</div><!-- //end heading -->
+
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="team-members">
+						<div class="team-avatar">
+							<img class="img-responsive" src="images/member1.png" alt="">
+						</div>
+						<div class="team-desc">
+							<h4>Paul Denvor</h4>
+							<span>Marketing</span>
 						</div>
 					</div>
 				</div>
+				<div class="col-sm-4">
+					<div class="team-members">
+						<div class="team-avatar">
+							<img class="img-responsive" src="images/member2.png" alt="">
+						</div>
+						<div class="team-desc">
+							<h4>Sarah Karkoz</h4>
+							<span>Founder</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="team-members">
+						<div class="team-avatar">
+							<img class="img-responsive" src="images/member3.png" alt="">
+						</div>
+						<div class="team-desc">
+							<h4>Steve Will</h4>
+							<span>Director</span>
+						</div>
+					</div>
+				</div>
+			</div><!-- //end row -->
+		</div>
+	</section>
 
-			</div>
-		</section>
-		<!-- Team -->
+	<!-- FOOTER -->
+	<?php include 'templates/footer.php' ?>
 
-		<!-- FOOTER -->
-		<?php include 'templates/footer.php' ?>
 
-		
-		
-		<!-- mouse over -->
-		<script>
-			$(function() {
-				$( "#show-option" ).tooltip({
-					show: {
-						effect: "slideDown",
-						delay: 300
-					}
-				});
+
+	<!-- mouse over -->
+	<script>
+		$(function() {
+			$( "#show-option" ).tooltip({
+				show: {
+					effect: "slideDown",
+					delay: 300
+				}
 			});
-		</script>
-		<!-- Bootstrap JS -->
+		});
+	</script>
+	<!-- Bootstrap JS -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+990DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-		<!-- App JS -->
-		<script type="text/javascript" src="js/login.js"></script>
-		<script src="js/app.js"></script>
+	<!-- App JS -->
+	<script type="text/javascript" src="js/login.js"></script>
+	<script src="js/app.js"></script>
 
-	</body>
-	</html>
+</body>
+</html>
